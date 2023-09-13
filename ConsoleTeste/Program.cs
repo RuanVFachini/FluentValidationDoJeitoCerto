061860e67309fe000232c38ad30c0c8cf8845301
Ruan Vinicius Fachini <ruanvfachini@gmail.com>
2023-09-12 21:54:14 -0300

    Criado projeto inicial

---
+++ ConsoleTeste/Program.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleTeste
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var validator = new Validator();
+
+            try
+            {
+                await validator.ValidateAndThrowAsync(new Li());
+            }
+            catch (FluentValidation.ValidationException ex)
+            {
+                ex.Errors.ToList().ForEach(e =>
+                {
+                    Console.WriteLine(e.ErrorMessage);
+                });
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ ConsoleTeste/Validator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTeste
+{
+    public class Validator : AbstractValidator<Li>
+    {
+        public Validator()
+        {
+            RuleFor(x => x)
+                .Cascade(CascadeMode.Stop)
+                .CustomAsync(async(li, context, token) =>
+            {
+                context.AddFailure(new FluentValidation.Results.ValidationFailure()
+                {
+                    ErrorMessage = "Validação 1 falhou",
+                    PropertyName = "Numero",
+                    CustomState = new
+                    {
+                        QualquerCoisa = 1
+                    }
+                });
+            }).CustomAsync(async(li, context, token) =>
+            {
+                context.AddFailure(new FluentValidation.Results.ValidationFailure()
+                {
+                    ErrorMessage = "Validação 2 falhou",
+                    PropertyName = "Numero",
+                    CustomState = new
+                    {
+                        QualquerCoisa = 2
+                    }
+                });
+            });
+        }
+    }
+}
